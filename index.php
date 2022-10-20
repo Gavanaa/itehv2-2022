7e6588a7d097813540eb431b10e6adb11c21a24f
Ana Gavovic <ana.gavovic.a@gmail.com>
2022-10-20 11:55:14 +0200

    dodata klasa

--- index.php
+++ index.php
@@ -1,3 +1,60 @@
 <?php
-echo "Hello world"; 
+require "model/users.php";
+require "dbBroker.php";
+session_start();
+if(isset($_POST["username"]) && isset($_POST['password'])){
+
+    $uname=$_POST["username"];
+    $upass=$_POST['password'];
+    $korisnik=new User(1,$uname,$upass);
+    $conn=new mysqli();
+    $odgovor=User::loginUser($korisnik,$conn);
+    if($odgovor->num_rows==1){
+        $_session["user_id"]=$korisnik->id;
+        header("Location: home.php");
+    }
+}
+
+
+
+
+
+
+
 ?>
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>FON: Zakazivanje kolokvijuma</title>
+
+</head>
+<body>
+    <div class="login-form">
+        <div class="main-div">
+            <form method="POST" action="#">
+                <div class="container">
+                    <label class="username">Korisnicko ime</label>
+                    <input type="text" name="username" class="form-control"  required>
+                    <br>
+                    <label for="password">Lozinka</label>
+                    <input type="password" name="password" class="form-control" required>
+                    <button type="submit" class="btn btn-primary" name="submit">Prijavi se</button>
+                </div>
+
+            </form>
+        </div>
+
+        
+    </div>
+</body>
+</html>
